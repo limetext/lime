b5b6724bb3d56a0f0c0e21c34f5753f3b0cf3331
ZiDoMs <zidom72@gmail.com>
2014-05-04 22:15:56 +0430

    Adding save and save_as command but there is a problem with view SaveAs method

---
+++ backend/commands/save.go
@@ -0,0 +1,43 @@
+// Copyright 2013 The lime Authors.
+// Use of this source code is governed by a 2-clause
+// BSD-style license that can be found in the LICENSE file.
+
+package commands
+
+import (
+	. "github.com/limetext/lime/backend"
+)
+
+type (
+	SaveCommand struct {
+		DefaultCommand
+	}
+
+	SaveAsCommand struct {
+		DefaultCommand
+		Name string
+	}
+)
+
+func (c *SaveCommand) Run(v *View, e *Edit) error {
+	err := v.Save()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *SaveAsCommand) Run(v *View, e *Edit) error {
+	err := v.SaveAs(c.Name)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func init() {
+	register([]cmd{
+		{"save", &SaveCommand{}},
+		{"save_as", &SaveAsCommand{}},
+	})
+}
