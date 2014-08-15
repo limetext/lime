baa62732743606e11ca8cf2c440c02f14e0df321
Kaoet <onetwogoo@live.com>
2014-08-15 13:19:30 +0800

    sort_lines and sort_selection command

---
+++ backend/commands/sort.go
@@ -0,0 +1,187 @@
+// Copyright 2014 The lime Authors.
+// Use of this source code is governed by a 2-clause
+// BSD-style license that can be found in the LICENSE file.
+
+package commands
+
+import (
+	. "github.com/limetext/lime/backend"
+	. "github.com/quarnster/util/text"
+	"sort"
+	"strings"
+)
+
+type (
+	// The SortLinesCommand sorts all lines
+	// intersecting a selection region
+	SortLinesCommand struct {
+		DefaultCommand
+		CaseSensitive    bool
+		Reverse          bool
+		RemoveDuplicates bool
+	}
+
+	// The SortSelectionCommand sorts contents
+	// of each selection region with respect to
+	// each other
+	SortSelectionCommand struct {
+		DefaultCommand
+		CaseSensitive    bool
+		Reverse          bool
+		RemoveDuplicates bool
+	}
+
+	// Helper type to sort Regions by theirs positions
+	regionSorter []Region
+
+	// Helper struct to sort strings
+	textSorter struct {
+		texts         []string
+		caseSensitive bool
+		reverse       bool
+	}
+)
+
+// regionSorter implements sort.Interface
+func (s regionSorter) Len() int { return len(s) }
+func (s regionSorter) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s regionSorter) Less(i, j int) bool {
+	return s[i].Begin() < s[j].Begin()
+}
+
+// stringSorter implements sort.Interface
+func (s textSorter) Len() int { return len(s.texts) }
+func (s textSorter) Swap(i, j int) {
+	s.texts[i], s.texts[j] = s.texts[j], s.texts[i]
+}
+func (s textSorter) Less(i, j int) bool {
+	textA := s.texts[i]
+	textB := s.texts[j]
+
+	if !s.caseSensitive {
+		textA = strings.ToLower(textA)
+		textB = strings.ToLower(textB)
+	}
+
+	if s.reverse {
+		return textA > textB
+	}
+	return textA < textB
+}
+
+func (c *SortLinesCommand) Run(v *View, e *Edit) error {
+	sel := v.Sel()
+	buf := v.Buffer()
+
+	// Used as a set of int
+	sorted_rows := make(map[int]struct{})
+
+	regions := []Region{}
+	texts := []string{}
+	for i := 0; i < sel.Len(); i++ {
+		// Get regions containing each line.
+		for _, r := range buf.Lines(sel.Get(i)) {
+			if _, ok := sorted_rows[r.Begin()]; !ok {
+				sorted_rows[r.Begin()] = struct{}{}
+				regions = append(regions, r)
+				texts = append(texts, buf.Substr(r))
+			}
+		}
+	}
+
+	sort.Sort(textSorter{
+		texts:         texts,
+		caseSensitive: c.CaseSensitive,
+		reverse:       c.Reverse,
+	})
+	texts = removeDuplicates(c.CaseSensitive, texts)
+
+	sort.Sort(regionSorter(regions))
+
+	offset := 0
+	for i, r := range regions {
+		r = Region{r.A + offset, r.B + offset}
+		if i < len(texts) {
+			v.Replace(e, r, texts[i])
+			offset += len(texts[i]) - r.Size()
+		} else {
+			// Erase the line and its ending
+			fullLine := buf.FullLineR(r)
+			v.Erase(e, fullLine)
+			offset -= fullLine.Size()
+		}
+	}
+
+	return nil
+}
+
+func (c *SortSelectionCommand) Run(v *View, e *Edit) error {
+	sel := v.Sel()
+	buf := v.Buffer()
+
+	regions := make([]Region, sel.Len())
+	texts := make([]string, sel.Len())
+	for i := 0; i < sel.Len(); i++ {
+		regions[i] = sel.Get(i)
+		texts[i] = buf.Substr(regions[i])
+	}
+
+	sort.Sort(textSorter{
+		texts:         texts,
+		caseSensitive: c.CaseSensitive,
+		reverse:       c.Reverse,
+	})
+	texts = removeDuplicates(c.CaseSensitive, texts)
+
+	sort.Sort(regionSorter(regions))
+
+	offset := 0
+	for i, r := range regions {
+		r = Region{r.A + offset, r.B + offset}
+		if i < len(texts) {
+			v.Replace(e, r, texts[i])
+			offset += len(texts[i]) - r.Size()
+		} else {
+			v.Erase(e, r)
+			offset -= r.Size()
+		}
+	}
+
+	return nil
+}
+
+// Remove duplicate ones from a sorted slice of string
+func removeDuplicates(caseSensitive bool, xs []string) []string {
+	var i, j int
+	for j < len(xs) {
+		var accept bool
+		if i > 0 {
+			prev := xs[i-1]
+			curr := xs[j]
+			if !caseSensitive {
+				prev = strings.ToLower(prev)
+				curr = strings.ToLower(curr)
+			}
+			accept = (prev != curr)
+		} else {
+			accept = true
+		}
+		if accept {
+			xs[i] = xs[j]
+			i++
+			j++
+		} else {
+			j++
+		}
+	}
+	return xs[:i]
+}
+
+func init() {
+	register([]cmd{
+		{"sort_lines", &SortLinesCommand{}},
+		{"sort_selection", &SortSelectionCommand{}},
+	})
+}
---
+++ backend/commands/sort_test.go
@@ -0,0 +1,142 @@
+// Copyright 2014 The lime Authors.
+// Use of this source code is governed by a 2-clause
+// BSD-style license that can be found in the LICENSE file.
+
+package commands
+
+import (
+	. "github.com/limetext/lime/backend"
+	. "github.com/quarnster/util/text"
+	"testing"
+)
+
+type test struct {
+	text             string
+	caseSensitive    bool
+	reverse          bool
+	removeDuplicates bool
+	sel              []Region
+	expect           string
+}
+
+func TestSortLines(t *testing.T) {
+	tests := []test{
+		{ // Case sensitive
+			"B\nc\na",
+			true,
+			false,
+			false,
+			[]Region{{0, 5}},
+			"B\na\nc",
+		},
+		{ // Case insensitive
+			"text\nSublime\nlime",
+			false,
+			false,
+			false,
+			[]Region{{0, 17}},
+			"lime\nSublime\ntext",
+		},
+		{ // Reverse
+			"b\nc\na",
+			true,
+			true,
+			false,
+			[]Region{{0, 5}},
+			"c\nb\na",
+		},
+		{ // Noncontinuous selection
+			"b\nc\na",
+			true,
+			false,
+			false,
+			[]Region{{0, 1}, {4, 5}},
+			"a\nc\nb",
+		},
+		{ // Remove duplicates
+			"a\nb\na",
+			true,
+			false,
+			true,
+			[]Region{{0, 5}},
+			"a\nb\n",
+		},
+	}
+
+	runSortTest(t, tests, "sort_lines")
+}
+
+func TestSortSelection(t *testing.T) {
+	tests := []test{
+		{ // Case sensitive
+			"Bca",
+			true,
+			false,
+			false,
+			[]Region{{0, 1}, {1, 2}, {2, 3}},
+			"Bac",
+		},
+		{ // Case insensitive
+			"textSublimelime",
+			false,
+			false,
+			false,
+			[]Region{{0, 4}, {4, 11}, {11, 15}},
+			"limeSublimetext",
+		},
+		{ // Reverse
+			"bca",
+			true,
+			true,
+			false,
+			[]Region{{0, 1}, {1, 2}, {2, 3}},
+			"cba",
+		},
+		{ // Noncontinuous selection
+			"bca",
+			true,
+			false,
+			false,
+			[]Region{{0, 1}, {2, 3}},
+			"acb",
+		},
+		{ // Remove duplicates
+			"aba",
+			true,
+			false,
+			true,
+			[]Region{{0, 1}, {1, 2}, {2, 3}},
+			"ab",
+		},
+	}
+
+	runSortTest(t, tests, "sort_selection")
+}
+
+func runSortTest(t *testing.T, tests []test, command string) {
+	ed := GetEditor()
+	w := ed.NewWindow()
+
+	for i, test := range tests {
+		v := w.NewFile()
+		e := v.BeginEdit()
+		v.Insert(e, 0, test.text)
+		v.EndEdit(e)
+
+		v.Sel().Clear()
+		for _, r := range test.sel {
+			v.Sel().Add(r)
+		}
+
+		args := map[string]interface{}{
+			"case_sensitive":    test.caseSensitive,
+			"reverse":           test.reverse,
+			"remove_duplicates": test.removeDuplicates,
+		}
+		ed.CommandHandler().RunTextCommand(v, command, args)
+
+		if d := v.Buffer().Substr(Region{0, v.Buffer().Size()}); d != test.expect {
+			t.Errorf("Test %d: Excepted %#v,\n but got %#v", i, test.expect, d)
+		}
+	}
+}
