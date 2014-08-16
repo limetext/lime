c3efefb7f81bb2cfaa740ccc458e29b2339bd0eb
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2014-08-16 22:06:55 +0100

    Add a TextCommand registration test.

--- backend/commandhandler_test.go
+++ backend/commandhandler_test.go
@@ -17,6 +17,10 @@ type (
 	DummyWindowCommand struct {
 		DefaultCommand
 	}
+
+	DummyTextCommand struct {
+		DefaultCommand
+	}
 )
 
 func (c *DummyApplicationCommand) Run() error {
@@ -28,6 +32,10 @@ func (c *DummyApplicationCommand) IsChecked() bool {
 }
 
 func (c *DummyWindowCommand) Run(w *Window) error {
+	return fmt.Errorf("Ran")
+}
+
+func (c *DummyTextCommand) Run(v *View, e *Edit) error {
 	return fmt.Errorf("Ran")
 }
 
@@ -79,16 +87,39 @@ func TestRegisterWindowCommand(t *testing.T) {
 	var w Window
 
 	name := "wnd_test_command"
-	ac := DummyWindowCommand{}
+	wc := DummyWindowCommand{}
 	ch := GetEditor().CommandHandler()
 
-	err := ch.Register(name, &ac)
+	err := ch.Register(name, &wc)
 
 	if err != nil {
 		t.Errorf("Got error while registering: %s", err)
 	}
 
 	err = ch.RunWindowCommand(&w, name, Args{})
+
+	if err == nil {
+		t.Errorf("Expected %s to run, but it didn't", name)
+	} else if err.Error() != "Ran" {
+		t.Errorf("Expected %s to run, but it got an error: %v", name, err)
+	}
+}
+
+func TestRegisterTextCommand(t *testing.T) {
+	ed := GetEditor()
+
+	name := "text_test_command"
+	tc := DummyTextCommand{}
+	ch := ed.CommandHandler()
+
+	err := ch.Register(name, &tc)
+
+	if err != nil {
+		t.Errorf("Got error while registering: %s", err)
+	}
+
+	v := ed.NewWindow().NewFile()
+	err = ch.RunTextCommand(v, name, Args{})
 
 	if err == nil {
 		t.Errorf("Expected %s to run, but it didn't", name)
