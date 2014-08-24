d1fbd3e923aa5566e446836401053b33ea7c30cb
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2014-08-24 11:38:48 +0100

    Add FindUnderExpand tests.
    Close limetext#109.

--- backend/commands/find_test.go
+++ backend/commands/find_test.go
@@ -31,9 +31,18 @@ func TestSingleSelection(t *testing.T) {
 	}
 
 	tests := []Test{
-		{[]Region{{1, 1}, {2, 2}, {3, 3}, {6, 6}}, []Region{{1, 1}}},
-		{[]Region{{2, 2}, {3, 3}, {6, 6}}, []Region{{2, 2}}},
-		{[]Region{{5, 5}}, []Region{{5, 5}}},
+		{
+			[]Region{{1, 1}, {2, 2}, {3, 3}, {6, 6}},
+			[]Region{{1, 1}},
+		},
+		{
+			[]Region{{2, 2}, {3, 3}, {6, 6}},
+			[]Region{{2, 2}},
+		},
+		{
+			[]Region{{5, 5}},
+			[]Region{{5, 5}},
+		},
 	}
 
 	for i, test := range tests {
@@ -43,7 +52,44 @@ func TestSingleSelection(t *testing.T) {
 		}
 		ed.CommandHandler().RunTextCommand(v, "single_selection", nil)
 		if sr := v.Sel().Regions(); !reflect.DeepEqual(sr, test.exp) {
-			t.Errorf("Single Selection test %d failed: %v, %+v", i, sr, test)
+			t.Errorf("Test %d failed: %v, %+v", i, sr, test)
+		}
+	}
+}
+
+func TestFindUnderExpand(t *testing.T) {
+	ed := GetEditor()
+
+	w := ed.NewWindow()
+	v := w.NewFile()
+	e := v.BeginEdit()
+	v.Insert(e, 0, "Hello World!\nTest123123\nAbrakadabra\n")
+	v.EndEdit(e)
+
+	type Test struct {
+		in  []Region
+		exp []Region
+	}
+
+	tests := []Test{
+		{
+			[]Region{{0, 0}},
+			[]Region{{0, 5}},
+		},
+		{
+			[]Region{{19, 20}},
+			[]Region{{19, 20}, {22, 23}},
+		},
+	}
+
+	for i, test := range tests {
+		v.Sel().Clear()
+		for _, r := range test.in {
+			v.Sel().Add(r)
+		}
+		ed.CommandHandler().RunTextCommand(v, "find_under_expand", nil)
+		if sr := v.Sel().Regions(); !reflect.DeepEqual(sr, test.exp) {
+			t.Errorf("Test %d failed: %v, %+v", i, sr, test)
 		}
 	}
 }
@@ -68,9 +114,18 @@ func TestSelectAll(t *testing.T) {
 	}
 
 	tests := []Test{
-		{[]Region{{1, 1}, {2, 2}, {3, 3}, {6, 6}}, []Region{{0, 36}}},
-		{[]Region{{2, 2}, {3, 3}, {6, 6}}, []Region{{0, 36}}},
-		{[]Region{{5, 5}}, []Region{{0, 36}}},
+		{
+			[]Region{{1, 1}, {2, 2}, {3, 3}, {6, 6}},
+			[]Region{{0, 36}},
+		},
+		{
+			[]Region{{2, 2}, {3, 3}, {6, 6}},
+			[]Region{{0, 36}},
+		},
+		{
+			[]Region{{5, 5}},
+			[]Region{{0, 36}},
+		},
 	}
 
 	for i, test := range tests {
@@ -80,7 +135,7 @@ func TestSelectAll(t *testing.T) {
 		}
 		ed.CommandHandler().RunTextCommand(v, "select_all", nil)
 		if sr := v.Sel().Regions(); !reflect.DeepEqual(sr, test.exp) {
-			t.Errorf("Select All test %d failed: %v, %+v", i, sr, test)
+			t.Errorf("Test %d failed: %v, %+v", i, sr, test)
 		}
 	}
 }
