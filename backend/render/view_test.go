a99b480b16087e2b716b9ff82b0ad0d3a95157a7
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2014-09-11 21:33:44 +0100

    Add ViewRegionMap tests.

--- backend/render/view_test.go
+++ backend/render/view_test.go
@@ -10,6 +10,100 @@ import (
 	"testing"
 )
 
+func TestViewRegionMapCull(t *testing.T) {
+	tests := []struct {
+		regionsA []text.Region
+		regionsB []text.Region
+		cull     text.Region
+		expA     []text.Region
+		expB     []text.Region
+	}{
+		{
+			[]text.Region{},
+			[]text.Region{},
+			text.Region{},
+			nil,
+			nil,
+		},
+		{
+			[]text.Region{{100, 200}},
+			[]text.Region{},
+			text.Region{},
+			nil,
+			nil,
+		},
+		{
+			[]text.Region{},
+			[]text.Region{},
+			text.Region{0, 50},
+			nil,
+			nil,
+		},
+		{
+			[]text.Region{{100, 200}},
+			[]text.Region{},
+			text.Region{0, 50},
+			nil,
+			nil,
+		},
+		{
+			[]text.Region{{100, 200}},
+			[]text.Region{{300, 400}},
+			text.Region{0, 50},
+			nil,
+			nil,
+		},
+		{
+			[]text.Region{{100, 200}},
+			[]text.Region{{300, 400}},
+			text.Region{0, 150},
+			[]text.Region{{100, 150}},
+			nil,
+		},
+		{
+			[]text.Region{{100, 200}},
+			[]text.Region{{300, 400}},
+			text.Region{150, 350},
+			[]text.Region{{150, 200}},
+			[]text.Region{{300, 350}},
+		},
+	}
+
+	for i, test := range tests {
+		vrm := ViewRegionMap{}
+
+		vrA := ViewRegions{}
+		vrA.Regions.AddAll(test.regionsA)
+		vrm["A"] = vrA
+
+		vrB := ViewRegions{}
+		vrB.Regions.AddAll(test.regionsB)
+		vrm["B"] = vrB
+
+		vrm.Cull(test.cull)
+
+		if test.expA != nil {
+			rA := vrA.Regions.Regions()
+
+			if !reflect.DeepEqual(rA, test.expA) {
+				t.Errorf("Test %d: Expected %s, but got %s", i, test.expA, rA)
+			}
+		} else if _, ok := vrm["A"]; ok {
+			t.Errorf("Test %d: Expected %s, but got %s", i, test.expA, vrm["A"])
+		}
+
+		if test.expB != nil {
+			rB := vrB.Regions.Regions()
+
+			if !reflect.DeepEqual(rB, test.expB) {
+				t.Errorf("Test %d: Expected %s, but got %s", i, test.expB, rB)
+			}
+		} else if _, ok := vrm["B"]; ok {
+			t.Errorf("Test %d: Expected %s, but got %s", i, test.expB, vrm["B"])
+		}
+	}
+}
+
 func TestViewRegionsCull(t *testing.T) {
 	tests := []struct {
 		regions []text.Region
