dc2e1824d5561ede1358619889052f0589f70511
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2014-09-20 14:19:17 +0100

    Close some things when we're done with them in the editor tests.

--- backend/editor_test.go
+++ backend/editor_test.go
@@ -25,8 +25,8 @@ func (d *DummyWatched) Reload() {
 }
 
 func TestGetEditor(t *testing.T) {
-	e := GetEditor()
-	if e == nil {
+	editor := GetEditor()
+	if editor == nil {
 		t.Error("Expected an editor, but got nil")
 	}
 }
@@ -132,16 +132,24 @@ func TestWatchOnSaveAs(t *testing.T) {
 			"testdata/User.sublime-settings",
 		},
 	}
-	ed := GetEditor()
-	w := ed.NewWindow()
+
+	editor := GetEditor()
+
+	w := editor.NewWindow()
+	// defer w.Close()
+
 	for i, test := range tests {
 		v := w.OpenFile(testfile, 0)
+		// defer v.Close()
+
 		if err := v.SaveAs(test.as); err != nil {
 			t.Fatalf("Test %d: Can't save to `%s`: %s", i, test.as, err)
 		}
-		if _, exist := ed.watchedFiles[test.as]; !exist {
+
+		if _, exist := editor.watchedFiles[test.as]; !exist {
 			t.Errorf("Test %d: Should watch %s file", i, test.as)
 		}
+
 		if err := os.Remove(test.as); err != nil {
 			t.Errorf("Test %d: Couldn't remove test file %s", i, test.as)
 		}
@@ -153,6 +161,7 @@ func TestWatchingSettings(t *testing.T) {
 	return
 
 	var path string = "testdata/Default.sublime-settings"
+
 	editor := GetEditor()
 	editor.loadSetting(NewPacket(path, editor.Settings()))
 
@@ -166,7 +175,9 @@ func TestWatchingSettings(t *testing.T) {
 	if err != nil {
 		t.Fatal("Error in writing to setting")
 	}
+
 	time.Sleep(time.Millisecond * 10)
+
 	if tab_size := editor.Settings().Get("tab_size").(float64); tab_size != 8 {
 		t.Errorf("Expected tab_size equal to 8, but got %v", tab_size)
 	}
@@ -178,48 +189,59 @@ func TestWatchingSettings(t *testing.T) {
 }
 
 func TestNewWindow(t *testing.T) {
-	ed := GetEditor()
-	l := len(ed.Windows())
-	_ = ed.NewWindow()
+	editor := GetEditor()
+	l := len(editor.Windows())
 
-	if len(ed.Windows()) != l+1 {
-		t.Errorf("Expected 1 window, but got %d", len(ed.Windows()))
+	w := editor.NewWindow()
+	defer w.Close()
+
+	if len(editor.Windows()) != l+1 {
+		t.Errorf("Expected 1 window, but got %d", len(editor.Windows()))
 	}
 }
 
 func TestRemoveWindow(t *testing.T) {
-	ed := GetEditor()
-	l := len(ed.Windows())
+	editor := GetEditor()
+	l := len(editor.Windows())
 
-	w := ed.NewWindow()
-	ed.remove(w)
+	w0 := editor.NewWindow()
+	defer w0.Close()
 
-	if len(ed.Windows()) != l {
-		t.Errorf("Expected the window to be removed, but %d still remain", len(ed.Windows()))
+	editor.remove(w0)
+
+	if len(editor.Windows()) != l {
+		t.Errorf("Expected the window to be removed, but %d still remain", len(editor.Windows()))
 	}
 
-	w = ed.NewWindow()
-	ed.NewWindow()
-	ed.remove(w)
+	w1 := editor.NewWindow()
+	defer w1.Close()
 
-	if len(ed.Windows()) != l+1 {
-		t.Errorf("Expected the window to be removed, but %d still remain", len(ed.Windows()))
+	w2 := editor.NewWindow()
+	defer w2.Close()
+
+	editor.remove(w1)
+
+	if len(editor.Windows()) != l+1 {
+		t.Errorf("Expected the window to be removed, but %d still remain", len(editor.Windows()))
 	}
 }
 
 func TestSetActiveWindow(t *testing.T) {
-	ed := GetEditor()
+	editor := GetEditor()
 
-	w1 := ed.NewWindow()
-	w2 := ed.NewWindow()
+	w1 := editor.NewWindow()
+	defer w1.Close()
 
-	if ed.ActiveWindow() != w2 {
+	w2 := editor.NewWindow()
+	defer w2.Close()
+
+	if editor.ActiveWindow() != w2 {
 		t.Error("Expected the newest window to be active, but it wasn't")
 	}
 
-	ed.SetActiveWindow(w1)
+	editor.SetActiveWindow(w1)
 
-	if ed.ActiveWindow() != w1 {
+	if editor.ActiveWindow() != w1 {
 		t.Error("Expected the first window to be active, but it wasn't")
 	}
 }
@@ -227,32 +249,32 @@ func TestSetActiveWindow(t *testing.T) {
 func TestSetFrontend(t *testing.T) {
 	f := DummyFrontend{}
 
-	ed := GetEditor()
-	ed.SetFrontend(&f)
+	editor := GetEditor()
+	editor.SetFrontend(&f)
 
-	if ed.Frontend() != &f {
-		t.Errorf("Expected a DummyFrontend to be set, but got %T", ed.Frontend())
+	if editor.Frontend() != &f {
+		t.Errorf("Expected a DummyFrontend to be set, but got %T", editor.Frontend())
 	}
 }
 
 func TestClipboard(t *testing.T) {
-	ed := GetEditor()
+	editor := GetEditor()
 	s := "test"
 
-	ed.SetClipboard(s)
+	editor.SetClipboard(s)
 
-	if ed.GetClipboard() != s {
-		t.Errorf("Expected %s to be on the clipboard, but got %s", s, ed.GetClipboard())
+	if editor.GetClipboard() != s {
+		t.Errorf("Expected %s to be on the clipboard, but got %s", s, editor.GetClipboard())
 	}
 }
 
 func TestHandleInput(t *testing.T) {
-	ed := GetEditor()
+	editor := GetEditor()
 	kp := KeyPress{Key: 'i'}
 
-	ed.HandleInput(kp)
+	editor.HandleInput(kp)
 
-	if ki := <-ed.keyInput; ki != kp {
+	if ki := <-editor.keyInput; ki != kp {
 		t.Errorf("Expected %s to be on the input buffer, but got %s", kp, ki)
 	}
 }
