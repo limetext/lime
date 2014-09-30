98c5868255af0eb7dea936021ad3173025b2ecc0
Marcel Patzwahl <marcel.patzwahl@gmail.com>
2014-09-30 19:37:26 +0200

    Update window_test.go

--- backend/commands/window_test.go
+++ backend/commands/window_test.go
@@ -25,15 +25,13 @@ func TestCloseAllViews(t *testing.T) {
 	w := ed.NewWindow()
 	defer w.Close()
 
-	l := len(w.Views())
-
 	ed.CommandHandler().RunWindowCommand(w, "new_file", nil)
 	ed.CommandHandler().RunWindowCommand(w, "new_file", nil)
 	ed.CommandHandler().RunWindowCommand(w, "new_file", nil)
 
 	ed.CommandHandler().RunWindowCommand(w, "close_all_views", nil)
 
-	if len(w.Views()) != l {
+	if len(w.Views()) != 0 {
 		t.Errorf("Expected %d views, but got %d", l, len(w.Views()))
 	}
 }
