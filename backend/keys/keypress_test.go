829ee0cd89c487238e56e351b47649398ec8dbc8
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2014-10-19 23:03:47 +0100

    Add some more KeyPress tests.

--- backend/keys/keypress_test.go
+++ backend/keys/keypress_test.go
@@ -8,6 +8,74 @@ import (
 	"testing"
 )
 
+func TestKeyPressIndex(t *testing.T) {
+	tests := []struct {
+		kp  KeyPress
+		exp int
+	}{
+		{
+			KeyPress{Key: 'a', Shift: false, Super: false, Alt: false, Ctrl: false},
+			int('a'),
+		},
+		{
+			KeyPress{Key: 'a', Shift: true, Super: false, Alt: false, Ctrl: false},
+			int('a') + shift,
+		},
+		{
+			KeyPress{Key: 'a', Shift: true, Super: true, Alt: false, Ctrl: false},
+			int('a') + shift + super,
+		},
+		{
+			KeyPress{Key: 'a', Shift: true, Super: true, Alt: true, Ctrl: false},
+			int('a') + shift + super + alt,
+		},
+		{
+			KeyPress{Key: 'a', Shift: true, Super: true, Alt: true, Ctrl: true},
+			int('a') + shift + super + alt + ctrl,
+		},
+	}
+
+	for i, test := range tests {
+		if test.kp.Index() != test.exp {
+			t.Errorf("Test %d: Expected %d, but got %d", i, test.exp, test.kp.Index())
+		}
+	}
+}
+
+func TestKeyPressIsCharacter(t *testing.T) {
+	tests := []struct {
+		kp  KeyPress
+		exp bool
+	}{
+		{
+			KeyPress{Key: 'a', Shift: false, Super: false, Alt: false, Ctrl: false},
+			true,
+		},
+		{
+			KeyPress{Key: 'a', Shift: true, Super: false, Alt: false, Ctrl: false},
+			true,
+		},
+		{
+			KeyPress{Key: 'a', Shift: false, Super: true, Alt: false, Ctrl: false},
+			false,
+		},
+		{
+			KeyPress{Key: 'a', Shift: false, Super: false, Alt: false, Ctrl: true},
+			false,
+		},
+		// {
+		// 	KeyPress{Key: F1, Shift: false, Super: false, Alt: false, Ctrl: false},
+		// 	false,
+		// },
+	}
+
+	for i, test := range tests {
+		if test.kp.IsCharacter() != test.exp {
+			t.Errorf("Test %d: Expected %v, but got %v", i, test.exp, test.kp.IsCharacter())
+		}
+	}
+}
+
 func TestKeyPressFix(t *testing.T) {
 	k := KeyPress{'A', false, false, false, false}
 	k.fix()
@@ -16,5 +84,12 @@ func TestKeyPressFix(t *testing.T) {
 	}
 	if !k.Shift {
 		t.Error("Expected the shift modifier to be active, but it wasn't")
+	}
+}
+
+func TestKeyPressString(t *testing.T) {
+	k := KeyPress{'a', true, true, false, false}
+	if k.String() != "super+shift+a" {
+		t.Errorf("Expected %q, but got %q", "super+shift+a", k.String())
 	}
 }
