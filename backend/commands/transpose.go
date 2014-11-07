e5b67a05e9e581e7ea3ab98e6757d12e1380c471
Geoff Hickey <trapgate@gmail.com>
2014-11-07 02:14:40 -0500

    Fix formatting of transpose sources (gofmt)

--- backend/commands/transpose.go
+++ backend/commands/transpose.go
@@ -5,48 +5,48 @@
 package commands
 
 import (
-    . "github.com/limetext/lime/backend"
-    . "github.com/limetext/text"
+	. "github.com/limetext/lime/backend"
+	. "github.com/limetext/text"
 )
 
 type (
-    // Transpose: Swap the characters on either side of the cursor,
-    // then move the cursor forward one character.
-    TransposeCommand struct {
-        DefaultCommand
-    }
+	// Transpose: Swap the characters on either side of the cursor,
+	// then move the cursor forward one character.
+	TransposeCommand struct {
+		DefaultCommand
+	}
 )
 
 func (c *TransposeCommand) Run(v *View, e *Edit) error {
-    /*
-    	Correct behavior of Transpose:
-    		- Swap the characters on either side of the cursor(s), then move
-    		  forward one character. If a region is selected, do nothing.
-    */
+	/*
+		Correct behavior of Transpose:
+			- Swap the characters on either side of the cursor(s), then move
+			  forward one character. If a region is selected, do nothing.
+	*/
 
-    if v.Sel().HasNonEmpty() {
-        return nil
-    }
+	if v.Sel().HasNonEmpty() {
+		return nil
+	}
 
-    rs := v.Sel().Regions()
-    for i := range rs {
-        r := rs[i]
-        if r.A == 0 || r.A >= v.Buffer().Size() {
-            continue
-        }
-        s := Region{r.A - 1, r.A + 1}
-        rns := v.Buffer().SubstrR(s)
-        rnd := make([]rune, len(rns))
-        rnd[0] = rns[1]
-        rnd[1] = rns[0]
-        v.Replace(e, s, string(rnd))
-    }
+	rs := v.Sel().Regions()
+	for i := range rs {
+		r := rs[i]
+		if r.A == 0 || r.A >= v.Buffer().Size() {
+			continue
+		}
+		s := Region{r.A - 1, r.A + 1}
+		rns := v.Buffer().SubstrR(s)
+		rnd := make([]rune, len(rns))
+		rnd[0] = rns[1]
+		rnd[1] = rns[0]
+		v.Replace(e, s, string(rnd))
+	}
 
-    return nil
+	return nil
 }
 
 func init() {
-    register([]Command{
-        &TransposeCommand{},
-    })
+	register([]Command{
+		&TransposeCommand{},
+	})
 }
--- backend/commands/transpose_test.go
+++ backend/commands/transpose_test.go
@@ -5,53 +5,53 @@
 package commands
 
 import (
-    . "github.com/limetext/lime/backend"
-    . "github.com/limetext/text"
-    "testing"
+	. "github.com/limetext/lime/backend"
+	. "github.com/limetext/text"
+	"testing"
 )
 
 func TestTranspose(t *testing.T) {
-    ed := GetEditor()
+	ed := GetEditor()
 
-    w := ed.NewWindow()
-    defer w.Close()
+	w := ed.NewWindow()
+	defer w.Close()
 
-    v := w.NewFile()
-    defer func() {
-        v.SetScratch(true)
-        v.Close()
-    }()
+	v := w.NewFile()
+	defer func() {
+		v.SetScratch(true)
+		v.Close()
+	}()
 
-    e := v.BeginEdit()
-    v.Insert(e, 0, "First&Second")
-    v.EndEdit(e)
+	e := v.BeginEdit()
+	v.Insert(e, 0, "First&Second")
+	v.EndEdit(e)
 
-    // Test with regions at the beginning and end of the buffer,
-    // and one beyond the buffer limits.
-    rs1 := []Region{{0, 0}, {1, 2}, {3, 3}, {5, 5}, {12, 12}, {20, 20}}
-    rs2 := []Region{{0, 0}, {3, 3}, {5, 5}, {12, 12}, {20, 20}}
+	// Test with regions at the beginning and end of the buffer,
+	// and one beyond the buffer limits.
+	rs1 := []Region{{0, 0}, {1, 2}, {3, 3}, {5, 5}, {12, 12}, {20, 20}}
+	rs2 := []Region{{0, 0}, {3, 3}, {5, 5}, {12, 12}, {20, 20}}
 
-    // This first should change nothing, because there's a selection with size
-    // > 0.
-    selectRegions(v, rs1)
-    ed.CommandHandler().RunTextCommand(v, "transpose", nil)
-    checkContents(t, v, 1, "First&Second")
+	// This first should change nothing, because there's a selection with size
+	// > 0.
+	selectRegions(v, rs1)
+	ed.CommandHandler().RunTextCommand(v, "transpose", nil)
+	checkContents(t, v, 1, "First&Second")
 
-    selectRegions(v, rs2)
-    ed.CommandHandler().RunTextCommand(v, "transpose", nil)
-    checkContents(t, v, 2, "Fisr&tSecond")
+	selectRegions(v, rs2)
+	ed.CommandHandler().RunTextCommand(v, "transpose", nil)
+	checkContents(t, v, 2, "Fisr&tSecond")
 }
 
 func selectRegions(v *View, rs []Region) {
-    v.Sel().Clear()
-    for _, r := range rs {
-        v.Sel().Add(r)
-    }
+	v.Sel().Clear()
+	for _, r := range rs {
+		v.Sel().Add(r)
+	}
 }
 
 func checkContents(t *testing.T, v *View, tnum int, expected string) {
-    b := v.Buffer().Substr(Region{0, v.Buffer().Size()})
-    if b != expected {
-        t.Error("Test %d: Expected %q but got %q", tnum, expected, b)
-    }
+	b := v.Buffer().Substr(Region{0, v.Buffer().Size()})
+	if b != expected {
+		t.Error("Test %d: Expected %q but got %q", tnum, expected, b)
+	}
 }
