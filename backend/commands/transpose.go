83ce8119fa6238f95a935310badfeaeada38c244
Geoff Hickey <trapgate@gmail.com>
2014-11-08 13:32:03 -0500

    Rewrite new transpose code.
    Rewrite the transpose function to more completely mimic ST3's behavior. Also
    add more tests.

--- backend/commands/transpose.go
+++ backend/commands/transpose.go
@@ -1,4 +1,4 @@
-// Copyright 2013 The lime Authors.
+// Copyright 2014 The lime Authors.
 // Use of this source code is governed by a 2-clause
 // BSD-style license that can be found in the LICENSE file.
 
@@ -20,26 +20,72 @@ type (
 func (c *TransposeCommand) Run(v *View, e *Edit) error {
 	/*
 		Correct behavior of Transpose:
-			- Swap the characters on either side of the cursor(s), then move
-			  forward one character. If a region is selected, do nothing.
+			- ...is actually surprisingly complicated.
+			- Transpose behaves differently depending on whether any non-empty
+			  region is selected.
+			- If there are no non-empty regions, it will swap the characters on
+			  either side of the cursor(s), then move all cursors forward one
+			  character.
+			- If one region is selected, do nothing.
+			- If a region is selected and there is another cursor position,
+			  expand the cursor to a word and swap them.
+			- If two regions are selected, swap them.
+			- If more than two regions are selected, rotate them forward.
+			- See transpose_test.go for examples.
 	*/
 
+	rsnew := RegionSet{}
+	rs := v.Sel().Regions()
+
 	if v.Sel().HasNonEmpty() {
-		return nil
+		// Build a list of transpose regions based on the current selected regions
+		trs := RegionSet{}
+		for _, r := range rs {
+			if r.Empty() {
+				trs.Add(v.Buffer().Word(r.A))
+			} else {
+				trs.Add(r)
+			}
+		}
+		if trs.Len() < 2 {
+			return nil
+		}
+
+		srcr := trs.Regions()[trs.Len()-1]
+		stxt := v.Buffer().Substr(srcr)
+		slen := srcr.Size()
+		for i := 0; i < trs.Len(); i++ {
+			r := trs.Regions()[i]
+			dtxt := v.Buffer().Substr(r)
+			dlen := r.Size()
+			v.Replace(e, r, stxt)
+			trs.Adjust(r.Begin()+1, slen-dlen)
+			rsnew.Add(Region{r.Begin(), r.Begin() + slen})
+			stxt, slen = dtxt, dlen
+		}
+
+	} else {
+		for i, r := range rs {
+			if i > 0 && r.A-1 == v.Sel().Regions()[i-1].A {
+				continue
+			}
+			rsnew.Add(Region{r.A + 1, r.B + 1})
+			if r.A == 0 || r.A >= v.Buffer().Size() {
+				continue
+			}
+			r1 := Region{r.A - 1, r.A}
+			r2 := Region{r.A, r.A + 1}
+			s1 := v.Buffer().Substr(r1)
+			s2 := v.Buffer().Substr(r2)
+			v.Replace(e, r1, s2)
+			v.Replace(e, r2, s1)
+		}
 	}
 
-	rs := v.Sel().Regions()
-	for i := range rs {
-		r := rs[i]
-		if r.A == 0 || r.A >= v.Buffer().Size() {
-			continue
-		}
-		s := Region{r.A - 1, r.A + 1}
-		rns := v.Buffer().SubstrR(s)
-		rnd := make([]rune, len(rns))
-		rnd[0] = rns[1]
-		rnd[1] = rns[0]
-		v.Replace(e, s, string(rnd))
+	// Rebuild the active selections
+	v.Sel().Clear()
+	for _, r := range rsnew.Regions() {
+		v.Sel().Add(Region{r.A, r.B})
 	}
 
 	return nil
