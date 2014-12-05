53122332adc39210e01c060aecbc30c13e981e02
Fredrik Ehnbom <fredrik@ehnbom.nu>
2014-12-05 12:10:39 +0100

    backend/log: Ran tasks/build/fix.go + remove test log output file.
    For #461

--- backend/log/filelogwriter_test.go
+++ backend/log/filelogwriter_test.go
@@ -1,13 +1,21 @@
+// Copyright 2014 The lime Authors.
+// Use of this source code is governed by a 2-clause
+// BSD-style license that can be found in the LICENSE file.
+
 package log_test
 
 import (
 	"github.com/limetext/lime/backend/log"
 	"github.com/limetext/log4go"
+	"os"
 	"testing"
 )
 
+const logfn = "some file"
+
 func TestNewFileLogWriter(t *testing.T) {
-	l := log.NewFileLogWriter("some file", true)
+	l := log.NewFileLogWriter(logfn, true)
+	defer os.Remove(logfn)
 	if l == nil {
 		t.Error("NewFileLogWriter produced a nil")
 	}
@@ -15,7 +23,8 @@ func TestNewFileLogWriter(t *testing.T) {
 }
 
 func TestFileLogWriterLogWrite(t *testing.T) {
-	l := log.NewFileLogWriter("some file", true)
+	l := log.NewFileLogWriter(logfn, true)
+	defer os.Remove(logfn)
 	logRecord := &log4go.LogRecord{}
 	l.LogWrite(logRecord) // Void function. Testing for coverage.
 	l.Close()
