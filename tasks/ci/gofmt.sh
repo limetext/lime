f62e2eead7ceab4d13b3c32ab8a683a6837e994e
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2014-12-06 20:56:43 +0000

    [ci] Use tabs in gofmt.sh.

--- tasks/ci/gofmt.sh
+++ tasks/ci/gofmt.sh
@@ -3,7 +3,7 @@
 fmt="$(find . ! \( -path './packages' -prune \) -type f -name '*.go' -print0 | xargs -0 gofmt -l )"
 
 if [ -n "$fmt" ]; then
-    echo "Unformatted Go source code:"
-    echo "$fmt"
-    exit 1
+	echo "Unformatted Go source code:"
+	echo "$fmt"
+	exit 1
 fi
