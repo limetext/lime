1aebdfbf37dbf00d7afd26bb1912b4ecaf96c415
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2014-12-06 18:42:56 +0000

    [ci] Rename a variable in lime.sh.

--- tasks/ci/lime.sh
+++ tasks/ci/lime.sh
@@ -25,13 +25,13 @@ function fold_end {
 
 function run_tests {
 	go test "$1" -covermode=count -coverprofile=tmp.cov
-	build_result=$?
+	test_result=$?
 	# Can't do race tests at the same time as coverage as it'll report
 	# lots of false positives
 	go test -race "$1"
-	let build_result=$build_result+$?
+	let test_result=$test_result+$?
 	echo -ne "${YELLOW}=>${RESET} test $1 - "
-	if [ "$build_result" == "0" ]; then
+	if [ "$test_result" == "0" ]; then
 		echo -e "${GREEN}SUCCEEDED${RESET}"
 	else
 		echo -e "${RED}FAILED${RESET}"
@@ -42,12 +42,12 @@ function test_all {
 	let a=0
 	for pkg in $(go list "./$1/..."); do
 		run_tests "$pkg"
-		let a=$a+$build_result
-		if [ "$build_result" == "0" ]; then
+		let a=$a+$test_result
+		if [ "$test_result" == "0" ]; then
 			sed 1d tmp.cov >> coverage.cov
 		fi
 	done
-	build_result=$a
+	test_result=$a
 }
 
 fold_start "get.cov" "get coverage tools"
@@ -66,12 +66,12 @@ ret=0
 
 fold_start "test.backend" "test backend"
 test_all "backend"
-let ret=$ret+$build_result
+let ret=$ret+$test_result
 fold_end "test.backend"
 
 fold_start "test.termbox" "test termbox"
 test_all "frontend/termbox"
-let ret=$ret+$build_result
+let ret=$ret+$test_result
 fold_end "test.termbox"
 
 if [ "$ret" == "0" ]; then
