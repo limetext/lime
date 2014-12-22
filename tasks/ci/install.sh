5af3ae7f9e789f18f9453d33ca12845a6c6c7793
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2014-12-22 15:48:17 +0000

    [ci] Pull the install steps out into a script.
    Simplify since iirc, python3.4 is now in the official Ubuntu PPAs.

---
+++ tasks/ci/install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# install go get dependencies
+sudo apt-get install -qq mercurial
+
+# install backend dependencies
+sudo apt-get install -qq libonig-dev python3.4 python3.4-dev
