f4ce173f655ed78889663007e00a5acaa3f542ce
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2014-12-22 17:08:48 +0000

    [ci] Try different versions of the qt5 packages.

--- tasks/ci/install.sh
+++ tasks/ci/install.sh
@@ -9,4 +9,4 @@ sudo apt-get update -qq
 sudo apt-get install -qq libonig-dev python3.4 python3.4-dev
 
 # install qml frontend dependencies
-sudo apt-get install -qq qtbase5-private-dev qtdeclarative5-private-dev
+sudo apt-get install -qq qtbase5-dev qtdeclarative5-dev
