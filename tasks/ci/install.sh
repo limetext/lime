ea3c11c5fa6db9c92463ce7ca6b22fe7bb729e84
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2014-12-22 15:57:32 +0000

    [ci] Add the deadsnakes PPA.
    I was wrong...

--- tasks/ci/install.sh
+++ tasks/ci/install.sh
@@ -4,4 +4,6 @@
 sudo apt-get install -qq mercurial
 
 # install backend dependencies
+echo 'yes' | sudo add-apt-repository ppa:fkrull/deadsnakes
+sudo apt-get update -qq
 sudo apt-get install -qq libonig-dev python3.4 python3.4-dev
