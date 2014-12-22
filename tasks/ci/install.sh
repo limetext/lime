8a4063329d87fd1b6c537b807f90e75d71a368c4
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2014-12-22 18:54:54 +0000

    [ci] Use the private-dev packages again.

--- tasks/ci/install.sh
+++ tasks/ci/install.sh
@@ -14,4 +14,4 @@ sudo apt-get install -qq libonig-dev python3.4 python3.4-dev
 # install qml frontend dependencies
 sudo add-apt-repository -y ppa:ubuntu-sdk-team/ppa
 sudo apt-get update -qq
-sudo apt-get install -qq qtbase5-dev qtdeclarative5-dev
+sudo apt-get install -qq qtbase5-private-dev qtdeclarative5-private-dev
