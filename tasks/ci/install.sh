04507e9660893bc62953d5d53eee6101a8c7bdcf
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2014-12-22 16:59:30 +0000

    [ci] Add some qt5 dependencies.

--- tasks/ci/install.sh
+++ tasks/ci/install.sh
@@ -7,3 +7,6 @@ sudo apt-get install -qq mercurial
 echo 'yes' | sudo add-apt-repository ppa:fkrull/deadsnakes
 sudo apt-get update -qq
 sudo apt-get install -qq libonig-dev python3.4 python3.4-dev
+
+# install qml frontend dependencies
+sudo apt-get install -qq qtbase5-private-dev qtdeclarative5-private-dev
