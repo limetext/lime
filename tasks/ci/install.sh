07b70d06389a40514997eb7d1ecf7b23e9699804
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2014-12-22 17:59:06 +0000

    [ci] Try a PPA for qt5.

--- tasks/ci/install.sh
+++ tasks/ci/install.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+# make sure we're up to date
+sudo apt-get update -qq
+
 # install go get dependencies
 sudo apt-get install -qq mercurial
 
 # install backend dependencies
-echo 'yes' | sudo add-apt-repository ppa:fkrull/deadsnakes
+sudo add-apt-repository -y ppa:fkrull/deadsnakes
 sudo apt-get update -qq
 sudo apt-get install -qq libonig-dev python3.4 python3.4-dev
 
 # install qml frontend dependencies
+sudo add-apt-repository -y ppa:ubuntu-sdk-team/ppa
+sudo apt-get update -qq
 sudo apt-get install -qq qtbase5-dev qtdeclarative5-dev
