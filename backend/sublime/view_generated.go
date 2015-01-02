40308095b3696815d61636e18a32e0dc177ce8cd
Mohammad Zolfaghari <zidom72@gmail.com>
2015-01-02 22:16:15 +0330

    updating copyright year for generated files

--- backend/sublime/view_generated.go
+++ backend/sublime/view_generated.go
@@ -1,4 +1,4 @@
-// Copyright 2014 The lime Authors.
+// Copyright 2015 The lime Authors.
 // Use of this source code is governed by a 2-clause
 // BSD-style license that can be found in the LICENSE file.
 
