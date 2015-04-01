edfa07e734c3f033e3b105add9067a4981ea0894
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2015-04-01 19:33:18 +0100

    [ci] Run install.sh through shellcheck.

--- tasks/ci/install.sh
+++ tasks/ci/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
 
 	# make sure we're up to date
 	sudo apt-get update -qq
@@ -18,12 +18,12 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
 	sudo apt-get update -qq
 	sudo apt-get install -qq qtbase5-private-dev qtdeclarative5-private-dev
 
-elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
+elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
 
 	brew update
 	brew install oniguruma python3 qt5
 	brew link --force qt5
-	ln -s $(brew --prefix python3)/Frameworks/Python.framework/Versions/3.4/lib/pkgconfig/* $(brew --prefix)/lib/pkgconfig
+	ln -s "$(brew --prefix python3)/Frameworks/Python.framework/Versions/3.4/lib/pkgconfig/*" "$(brew --prefix)/lib/pkgconfig"
 
 else
 
